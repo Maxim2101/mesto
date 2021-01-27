03bad23f82dbe64aea20fc2752ded39a7335156f
Maxim Marjin <maximmarjin@yandex.ru>
2021-01-27 18:42:05 +0300

    final

--- main.js
+++ main.js
@@ -1,30 +1,29 @@
-let popup = document.querySelector('.popup')
-let popupOn = document.querySelector('.profile__EditButton')
-let popupClose = document.querySelector('.popup__close')
+let popup = document.querySelector(".popup");
+let popupOn = document.querySelector(".profile__edit-button");
+let popupClose = document.querySelector(".popup__close");
 
-function togglePopup (evt) {
-evt.preventDefault()
-popup.classList.toggle('popup__opened')
+function togglePopup(evt) {
+    evt.preventDefault();
+    popup.classList.toggle("popup__opened");
 }
 
-popupOn.addEventListener('click', togglePopup)
-popupClose.addEventListener('click', togglePopup)
+popupOn.addEventListener("click", togglePopup);
+popupClose.addEventListener("click", togglePopup);
 
-let profileBtn = document.querySelector('.popup__SubmitButton')
-let nameInput = document.querySelector('.popup__change-name')
-let jobInput = document.querySelector('.popup__change-job')
+let profileBtn = document.querySelector(".popup__submit-button");
+let nameInput = document.querySelector(".popup__change-name");
+let jobInput = document.querySelector(".popup__change-job");
 
-function formSubmitHandler (evt) {
-evt.preventDefault();
+function formSubmitHandler(evt) {
+    evt.preventDefault();
 
-let name = document.querySelector(".profile__title")
-let job = document.querySelector(".profile__subtitle")
+    let name = document.querySelector(".profile__title");
+    let job = document.querySelector(".profile__subtitle");
 
-name.textContent= nameInput.value
-job.textContent=jobInput.value
-console.log("kkk")
-// popupClose.togglePopup()
-
+    name.textContent = nameInput.value;
+    job.textContent = jobInput.value;
+    console.log("kkk");
+    // popupClose.togglePopup()
 }
-profileBtn.addEventListener('click', formSubmitHandler);
-profileBtn.addEventListener('click', togglePopup);
+profileBtn.addEventListener("click", formSubmitHandler);
+profileBtn.addEventListener("click", togglePopup);

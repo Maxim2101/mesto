b9da3a661b1d20a91f0b202cdb7f44dbaaa51bbf
Maxim Marjin <maximmarjin@yandex.ru>
2021-02-01 22:37:36 +0300

    final

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,16 @@
 let popup = document.querySelector(".popup");
 let popupOn = document.querySelector(".profile__edit-button");
 let popupClose = document.querySelector(".popup__close");
-let name = document.querySelector(".profile__title");
-let job = document.querySelector(".profile__subtitle");
+let form = document.querySelector(".popup__admin")
 
 function togglePopup(evt) {
     evt.preventDefault();
+    let name = document.querySelector(".profile__title");
+    let job = document.querySelector(".profile__subtitle");
+    let heading = name.textContent
+    let subheading = job.textContent
+    nameInput.setAttribute("placeholder", heading)
+    jobInput.setAttribute("placeholder", subheading)
     popup.classList.toggle("popup__opened");
 }
 
@@ -18,13 +23,12 @@ let jobInput = document.querySelector(".popup__change-job");
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
-
-    
-
+    let name = document.querySelector(".profile__title");
+    let job = document.querySelector(".profile__subtitle");
     name.textContent = nameInput.value;
     job.textContent = jobInput.value;
     console.log("kkk");
     // popupClose.togglePopup()
 }
-profileBtn.addEventListener("click", formSubmitHandler);
-profileBtn.addEventListener("click", togglePopup);
+form.addEventListener("submit", formSubmitHandler);
+form.addEventListener("submit", togglePopup);

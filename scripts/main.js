b21fd19d4ec3b58949656d92474cd258a1b8349f
Maxim Marjin <maximmarjin@yandex.ru>
2021-03-24 18:36:47 +0300

    final

--- scripts/main.js
+++ scripts/main.js
@@ -11,13 +11,11 @@ const subNameImage = document.querySelector('.popup__input_pic')
 
 const editProfile = document.querySelector('.profile__edit-button');
 const editProfilePopup = document.querySelector('.popup_type_profile');
-const editProfilePopupClose = document.querySelector('.popup__closes');
 const editProfileForm = document.querySelector('.popup__admin');
 const profileForm = document.querySelector('.popup__admin_profile')
 const pictureForm = document.querySelector('.popup__admin_picture')
 
 const figure = document.querySelector('.popup_type_figure');
-const figureClose = document.querySelector('.popup__closes-figure');
 const figureImage = document.querySelector('.popup__photo-figure');
 const figureImageSub = document.querySelector('.popup__input-figure');
 
@@ -25,24 +23,26 @@ const elements = document.querySelector('.elements')
 
 const addElement = document.querySelector('.profile__add-button-container');
 const formItem = document.querySelector('.popup_type_picture');
-const closeFormItem = document.querySelector('.popup__closes-picture');
 
 const overlayPopupFigure = document.querySelector('.popup__overlay_figure');
-
-
 
 function closePopup (element) {
   element.classList.remove('popup_opened');
   document.removeEventListener('keydown', closeByEscape); 
 }
 
-const handleClosePopup = (event) => {
-  if (event.target === event.currentTarget){
-    closePopup(editProfilePopup);
-    closePopup(formItem);
-    closePopup(figure);
-  }
-};
+const popups = document.querySelectorAll('.popup')
+
+popups.forEach((popup) =>{
+  popup.addEventListener('click', (event)=> {
+    if(event.target.classList.contains('popup_opened')){
+      closePopup(popup)
+    }
+    if(event.target.classList.contains('popup__closes')){
+      closePopup(popup)
+    }
+  })
+})
 
 function openPopup (element) {
   element.classList.add('popup_opened');
@@ -56,7 +56,8 @@ function closeByEscape(evt) {
   }
 }
 
-const launchPopupImage = () => {
+const openProfilePopup = () => {
+  editProfileForm.reset()
   heading.value = userName.textContent;
   subheading.value = userJob.textContent;
   openPopup(editProfilePopup)
@@ -75,48 +76,45 @@ function openImage (name, link) {
   figureImageSub.textContent = name;
 }
 
-
-function addItemFormSubmit(event){
-  event.preventDefault()
-  const card = new Card({name: nameImage.value, link: subNameImage.value},'.element-template', openImage)
-  const cardElement = card.generateCard();
-  elements.prepend(cardElement);
-  nameImage.value = '';
-  subNameImage.value = '';
-  pictureForm.reset()
-  handleClosePopup(formItem);
-}
-
-initialCards.forEach((item) => {
+function createCard(item) {
   const card = new Card(item, '.element-template', openImage);
   const cardElement = card.generateCard();
   elements.prepend(cardElement);
-});
+}
+
+function addItemFormSubmit(event){
+  event.preventDefault()
+  // const card = new Card({name: nameImage.value, link: subNameImage.value},'.element-template', openImage)
+  // const cardElement = card.generateCard();
+  // elements.prepend(cardElement);
+  createCard({name: nameImage.value, link: subNameImage.value})
+  pictureForm.reset();
+  closePopup(formItem)
+}
+
+ initialCards.forEach((item) => {
+   // const card = new Card(item, '.element-template', openImage);
+   // const cardElement = card.generateCard();
+   // elements.prepend(cardElement);
+   createCard(item)
+ });
+
 
 editProfileForm.addEventListener ('submit', onSubmitProfilePopup);
 pictureForm.addEventListener ('submit', addItemFormSubmit);
 
-figureClose.addEventListener('click', function () {
-  closePopup(figure)
-});
-
 addElement.addEventListener('click', function () {
+  pictureForm.reset()
   openPopup(formItem)
 });
-closeFormItem.addEventListener('click', function () {
-  closePopup(formItem)
+
+editProfile.addEventListener('click', openProfilePopup);
+
+overlayPopupFigure.addEventListener('click', function (event){
+  if(event.target === event.currentTarget){
+    closePopup(figure);
+  }
 });
-
-editProfilePopupClose.addEventListener('click', function () {
-  closePopup(editProfilePopup)
-});
-
-
-editProfile.addEventListener('click', launchPopupImage);
-
-editProfilePopup.addEventListener('click', handleClosePopup);
-formItem.addEventListener('click', handleClosePopup);
-overlayPopupFigure.addEventListener('click', handleClosePopup);
 
 const sectionValidity = ({
   formSelector: '.popup__admin',
